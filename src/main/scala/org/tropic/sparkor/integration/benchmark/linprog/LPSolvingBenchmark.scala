15e39980124ba89fa89853ce40e00f17e84bbeb9
xliang <xin.liang@insa-rouen.fr>
2016-05-13 14:11:05 +0200

    change option of disableLpSolve

--- src/main/scala/org/tropic/sparkor/integration/benchmark/linprog/LPSolvingBenchmark.scala
+++ src/main/scala/org/tropic/sparkor/integration/benchmark/linprog/LPSolvingBenchmark.scala
@@ -54,64 +54,58 @@ object LPSolvingBenchmark extends App {
     mse / result.length
   }
 
-  /**
-    * Integration test to compare the solutions between OscaR and spark-or
-    *
-    * //@param args a option to choose solve with lpSolver (to entry something) or not
-    */
-  //def main(args: Array[String]): Unit = {
 
-    val option: Boolean = args.length > 0
 
-    val m = Array(5, 10, 50, 100, 210, 500)
-    val n = Array(3, 18, 40, 90, 250, 500)
-    val maxInt = 10
+  val disableLpSolve: Boolean = args.length > 0 && args(0) != "--disable-lpsolve"
 
-    var solution1: Vector = null
-    var score1: Double = 0
-    var t1: Double = 0
+  val m = Array(5, 20, 50, 100, 250, 500)
+  val n = Array(3, 10, 40, 90, 200, 400)
+  val maxInt = 10
 
-    val lp2 = new SparkOrSolver()
+  var solution1: Vector = null
+  var score1: Double = 0
+  var t1: Double = 0
 
-    for (i <- n.indices) {
-      val param = new LPGeneration(m(i), n(i), maxInt)
-      if (option) {
-        val lp = new LPSolver()
-        val (newA, newC) = lp.addZeros(param.A, param.c)
-        val res = solveLinearProblem {
-          lp.solve(newA, param.b, newC, ConstraintType.GreaterThan)
-        }
-        solution1 = res._1
-        score1 = res._2
-        t1 = res._3
+  val lp2 = new SparkOrSolver()
+
+  for (i <- n.indices) {
+    val param = new LPGeneration(m(i), n(i), maxInt)
+    if (disableLpSolve) {
+      val lp = new LPSolver()
+      val (newA, newC) = lp.addZeros(param.A, param.c)
+      val res = solveLinearProblem {
+        lp.solve(newA, param.b, newC, ConstraintType.GreaterThan)
       }
-      val (solution2, score2, t2) = solveLinearProblem {
-        lp2.solve(MatrixUtils.arrayToMatrix(param.A), param.b, param.c, ConstraintType.GreaterThan)
-      }
-
-      // Print solution
-      if (m(i) < 10 || n(i) < 10) {
-        println("\nParameters:")
-        println("A:")
-        for (i <- param.A.indices) println(param.A(i))
-        println("b:\n" + param.b)
-        println("c:\n" + param.c)
-        if (option)
-          println("\nSolution of lp_solve:" + solution1)
-        println("Solution of spark-or:" + solution2)
-      }
-      println("****************************************************************")
-      println("nbRows = " + n(i) + " & nbCols = " + m(i))
-      if (option)
-        println("\nValue of objective function with lp_solve: " + score1)
-      println("Value of objective function with spark-or: " + score2)
-      if (option)
-        println("\nElapsed time of lp_solve: " + t1 + " s")
-      println("Elapsed time of spark-or: " + t2 + " s")
-      if (option)
-        println("\nMean square error : " + mse(solution1, solution2))
-      println("****************************************************************")
-
+      solution1 = res._1
+      score1 = res._2
+      t1 = res._3
     }
-  //}
+    val (solution2, score2, t2) = solveLinearProblem {
+      lp2.solve(MatrixUtils.arrayToMatrix(param.A), param.b, param.c, ConstraintType.GreaterThan)
+    }
+
+    // Print solution
+    if (m(i) < 10 || n(i) < 10) {
+      println("\nParameters:")
+      println("A:")
+      for (i <- param.A.indices) println(param.A(i))
+      println("b:\n" + param.b)
+      println("c:\n" + param.c)
+      if (disableLpSolve)
+        println("\nSolution of lp_solve:" + solution1)
+      println("Solution of spark-or:" + solution2)
+    }
+    println("****************************************************************")
+    println("nbRows = " + n(i) + " & nbCols = " + m(i))
+    if (disableLpSolve)
+      println("\nValue of objective function with lp_solve: " + score1)
+    println("Value of objective function with spark-or: " + score2)
+    if (disableLpSolve)
+      println("\nElapsed time of lp_solve: " + t1 + " s")
+    println("Elapsed time of spark-or: " + t2 + " s")
+    if (disableLpSolve)
+      println("\nMean square error : " + mse(solution1, solution2))
+    println("****************************************************************")
+
+  }
 }
